147566c44a6a071add719ef0281b66703693633f
oxid322 <94226989+oxid322@users.noreply.github.com>
2025-02-28 23:35:42 +1000

    README.md

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
+sleep 5
+
+# Применение миграций
 python3 manage.py migrate --noinput
 
+# Сбор статических файлов
 python3 manage.py collectstatic --noinput
 
+# Копирование статических файлов
 cp -r /app/collected_static/. /backend_static/static/
 
-exec gunicorn --bind 0.0.0.0:80 backend.wsgi:application
+# Запуск Gunicorn
+exec gunicorn --bind 0.0.0.0:80 backend.wsgi
